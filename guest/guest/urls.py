2ad0b4382b9bcc4a361f0089aa1aa68f756a0c26
fan <pod1019@163.com>
2019-12-03 22:54:13 +0800

    修改一下  导入应用views文件的路径。由from sign import 改成from guest.sign import views

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 # from django.urls import path
 from django.conf.urls import url
-from sign import views #导入sign应用views文件
+from guest.sign import views #导入sign应用views文件
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
